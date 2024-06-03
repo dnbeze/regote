7aa83aa93cccb0827cdcd2ab8718384d719c1b1e
Brett Ezell <dnbeze55@gmail.com>
2024-06-03 15:27:54 -0400

    modified navbar

--- regote-frontend/src/components/Notes.js
+++ regote-frontend/src/components/Notes.js
@@ -3,7 +3,7 @@ const Notes = () => {
     return(
         <>
             <div className="has-text-centered">
-                <h2>This is for doing some note takin!</h2>
+                <h2>Notes!</h2>
                 <hr />
             </div>
         </>
