434de46dffe219b88b01770bf08c0de7a950aef0
Brett Ezell <dnbeze55@gmail.com>
2024-06-03 23:46:31 -0400

    middlwares cors config

--- regote-backend/cmd/api/middleware.go
+++ regote-backend/cmd/api/middleware.go
@@ -8,4 +8,10 @@ import (
 func initializeMiddleware(e *echo.Echo) {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowCredentials: true,
+	}))
 }
