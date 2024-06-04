27f722927c67eb540f287e1a4b8223aa01a71bad
Brett Ezell <dnbeze55@gmail.com>
2024-06-03 22:03:03 -0400

    handling basic json

--- regote-backend/cmd/api/handlers.go
+++ regote-backend/cmd/api/handlers.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
 )
 
 func (app *application) Home(c echo.Context) error {
-	return c.String(200, app.Domain)
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "regote is up and running",
+		Version: "0.0.1",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Response().Header().Set("Content-Type", "application/json")
+	c.Response().WriteHeader(http.StatusOK)
+	_, err = c.Response().Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
 }
